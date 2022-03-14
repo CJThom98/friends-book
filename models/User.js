8c38db01df93d126bf34e327be7cc227f83905ba
Cory Thompson <cory.thompson98@gmail.com>
2022-03-14 14:37:58 -0500

    uncommented

--- models/User.js
+++ models/User.js
@@ -1,48 +1,48 @@
-// const { Schema, model } = require('mongoose');
-// const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 
-// const UserSchema = new Schema({
-//     userName: {
-//         type: String,
-//         unique: true,
-//         required: true,
-//         trim: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         unique: true,
-//         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address.']
-//     },
-//     thoughts: [
-//         {
-//             type: Schema.Types.ObjectId,
-//             ref: 'Thought'
-//         }
-//     ],
-//     friends: [
-//         {
-//             type: Schema.Types.ObjectId,
-//             ref: 'User'
-//         }
-//     ]
-// },
-// {
-//     toJSON: {
-//         virtuals: true,
-//         getters: true
-//     },
-//     id: false
-// }
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address.']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
 
-// );
+);
 
-// const User = model('User', UserSchema);
+const User = model('User', UserSchema);
 
-// UserSchema.virtual('friendCount').get(function() {
-//     return this.friends.length;
-// });
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
-// module.exports = User;
+module.exports = User;
