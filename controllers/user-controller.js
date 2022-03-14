7ef3af0c2de5f273605fb3e42f11566b24c95ec6
Cory Thompson <cory.thompson98@gmail.com>
2022-03-14 14:53:56 -0500

    user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -46,6 +46,28 @@ const userController = {
     },
 
     updateUser({ params, body }, res) {
-        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators})
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
     }
-}
+};
+
+module.exports = userController;
