98be660f7c781321a7e068834bac0af225c7ad45
Cory Thompson <cory.thompson98@gmail.com>
2022-03-15 14:26:45 -0500

    friend routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,21 +4,26 @@ const {
     getAllUsers,
     getUserById,
     createUser,
+    newFriend,
     updateUser,
-    deleteUser
+    deleteUser,
+    removeFriend
   } = require('../../controllers/user-controller');
 
-// Set up GET all and POST at /api/users
 router
   .route('/')
   .get(getAllUsers)
   .post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/users/:id
 router
   .route('/:id')
   .get(getUserById)
   .put(updateUser)
   .delete(deleteUser);
+
+router
+  .router('/:userId/friends/:friendId')
+  .post(newFriend)
+  .delete(removeFriend)
 
 module.exports = router;
