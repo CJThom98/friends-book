d8e976b680ea653ac170775273ea612c9cf7075f
Cory Thompson <cory.thompson98@gmail.com>
2022-03-15 14:58:57 -0500

    thought model

---
+++ models/Thought.js
@@ -0,0 +1,69 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ResponseSchema = new Schema(
+    {
+        responseId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        responseBody: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createAt: {
+            type: Date,
+            default: Date.now,
+            get: createAtVal => moment(createAtVal).format('MM DD, YYYY [at] hh:mm')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createAt: {
+            type: Date,
+            default: Date.now,
+            get: createAtVal => moment(createAtVal).format('MM DD, YYYY [at] hh:mm')
+        },
+        username: {
+            type: String,
+            require: true,
+            ref: 'User'
+        },
+        responses: [ResponseSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
+
+const Thought = model('Thought', ThoughtSchema);
+
+ThoughtSchema.virtual('responseCount').get(function() {
+    return this.responses.length;
+});
+
+module.exports = Thought;
